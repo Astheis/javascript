eed4542c0d2b60de40d2fc1abfd1d4c4ed29ca2d
amandine <amandinegrisot@hotmail.com>
2020-02-18 11:41:31 +0100

    amelioration

--- main.js
+++ main.js
@@ -1,29 +1,4 @@
-let myAge = 35;
-let myYearOfBirth = 1984;
-let addition = myAge + myYearOfBirth;
-console.log(addition);
 
-
-let myName = 'Amandine';
-let mySurname = 'Grisot';
-let nameSurname = myName + ' ' + mySurname;
-console.log(nameSurname);
-
-var whichName = '' ;
-
-switch (whichName) {
-    case 'Sarah':
-        alert('Sarah Connor ?');
-    case 'Siri':
-        alert('Je n’ai pas compris votre question ?');
-    case 'Amandine':
-        alert('is that you ?');
-    case 'Max':
-        alert('He is the professor');
-default:
-        alert("oh! c'est un joli prénom");
-        break;
-}
 
 var whichAge = '';
 
@@ -38,3 +13,34 @@ default:
         alert('oh! vous avez') + (whichAge);
         break;
 }
+
+var whichName = prompt("quel est votre nom ?");
+
+switch (whichName) {
+    case 'Sarah':
+        alert('Sarah Connor ?');
+    case 'Siri':
+        alert('Je n’ai pas compris votre question ?');
+    case 'Amandine':
+        alert('is that you ?');
+    case 'Max':
+        alert('You are our Js mentor');
+default:
+        alert("oh! c'est un joli prénom");
+        break;
+}
+
+
+let myAge = 35;
+let myYearOfBirth = 1984;
+let addition = myAge + myYearOfBirth;
+console.log(addition);
+
+
+let myName = 'Amandine';
+let mySurname = 'Grisot';
+let nameSurname = myName + ' ' + mySurname;
+console.log(nameSurname);
+
+
+
