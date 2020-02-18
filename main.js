f18bf9a69ca9c4cdfa8f8f0b672fe58eef383558
amandine <amandinegrisot@hotmail.com>
2020-02-18 10:51:30 +0100

    exo 2

--- main.js
+++ main.js
@@ -2,3 +2,8 @@ let myAge = 35;
 let myYearOfBirth = 1984;
 let addition = myAge + myYearOfBirth;
 console.log(addition);
+
+let myName = 'Amandine';
+let mySurname = 'Grisot';
+let nameSurname = myName + ' ' + mySurname;
+console.log(nameSurname);
