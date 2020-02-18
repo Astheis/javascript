548f6db217aec7f2b4e599019ca307d78724fde0
amandine <amandinegrisot@hotmail.com>
2020-02-18 11:11:14 +0100

    debut exo 3

--- main.js
+++ main.js
@@ -3,7 +3,31 @@ let myYearOfBirth = 1984;
 let addition = myAge + myYearOfBirth;
 console.log(addition);
 
+
 let myName = 'Amandine';
 let mySurname = 'Grisot';
 let nameSurname = myName + ' ' + mySurname;
 console.log(nameSurname);
+
+
+
+var whichName = ''
+
+switch (test) {
+    case 'Sarah':
+        alert('Sarah Connor ?');
+
+    case 'Siri':
+        alert('Je n’ai pas compris votre question ?');
+
+    case 'Amandine':
+        alert('is that you ?');
+    
+    case 'Max':
+        alert('He is the professor');
+
+    
+default:
+        alert("oh! c'est un joli prénom");
+        break;
+}
