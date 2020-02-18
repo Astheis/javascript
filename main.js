0e168d170e7850c6fe6e3a3668a30ae18880a84a
amandine <amandinegrisot@hotmail.com>
2020-02-18 11:27:25 +0100

    switch 1.2

--- main.js
+++ main.js
@@ -9,25 +9,32 @@ let mySurname = 'Grisot';
 let nameSurname = myName + ' ' + mySurname;
 console.log(nameSurname);
 
-
-
 var whichName = '' ;
 
 switch (whichName) {
     case 'Sarah':
         alert('Sarah Connor ?');
-
     case 'Siri':
         alert('Je n’ai pas compris votre question ?');
-
     case 'Amandine':
         alert('is that you ?');
-    
     case 'Max':
         alert('He is the professor');
-
-    
 default:
         alert("oh! c'est un joli prénom");
+        break;
+}
+
+var whichAge = '';
+
+switch (whichAge) {
+    case '18':
+        alert('bravo, vous êtes majeur(e)!');
+    case '42':
+        alert("c'est la rép universelle");
+    case '100':
+        alert("incroyable, vous êtes centenaire !!");
+default:
+        alert('oh! vous avez') + (whichAge);
         break;
 }
